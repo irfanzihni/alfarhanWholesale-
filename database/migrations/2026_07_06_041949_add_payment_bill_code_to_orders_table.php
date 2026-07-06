1851e8bd873501533acbb8c4b614045e08a1e8dd
irfanzihni <irfanzabidi12gmail.com>
2026-07-06 17:42:32 +0800

    logo icon website

--- database/migrations/2026_07_06_041949_add_payment_bill_code_to_orders_table.php
+++ database/migrations/2026_07_06_041949_add_payment_bill_code_to_orders_table.php
@@ -9,20 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('orders', function (Blueprint $table) {
-            //
-        });
-    }
+    public function up()
+        {
+            Schema::table('orders', function (Blueprint $col) {
+                $col->string('payment_bill_code')->nullable()->after('id'); // letak lepas id atau mana-mana kolum sesuai
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('orders', function (Blueprint $table) {
-            //
-        });
-    }
+    public function down()
+        {
+            Schema::table('orders', function (Blueprint $col) {
+                $col->dropColumn('payment_bill_code');
+            });
+        }
 };
